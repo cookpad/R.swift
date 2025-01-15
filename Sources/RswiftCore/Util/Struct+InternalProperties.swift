4c9eca2fcce065629529737a6e076c6cb7e5514f
ainame <s.namai.09@gmail.com>
2025-01-15 12:14:06 +0000

    Support Bundle.module in hostingBundle option

--- Sources/RswiftCore/Util/Struct+InternalProperties.swift
+++ Sources/RswiftCore/Util/Struct+InternalProperties.swift
@@ -11,7 +11,10 @@ import Foundation
 extension Struct {
   func addingInternalProperties(forBundleIdentifier bundleIdentifier: String, hostingBundle: String? = nil) -> Struct {
     let hostingBundleValue: String
-    if let bundleName = hostingBundle, !bundleName.isEmpty {
+    // This case is extended by the fork cookpad/R.swift to support SPM
+    if let bundleName = hostingBundle, bundleName == "Bundle.module" {
+      hostingBundleValue = "Bundle.module"
+    } else if let bundleName = hostingBundle, !bundleName.isEmpty {
       hostingBundleValue = "Bundle(for: R.Class.self).path(forResource: \"\(bundleName)\", ofType: \"bundle\").flatMap(Bundle.init(path:)) ?? Bundle(for: R.Class.self)"
     } else {
       hostingBundleValue = "Bundle(for: R.Class.self)"
